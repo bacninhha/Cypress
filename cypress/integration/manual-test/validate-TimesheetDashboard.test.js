5b06af0700e5a89e58b25a4f489dfb5b852590d2
bacninhha <99166970+bacninhha@users.noreply.github.com>
2022-04-12 10:48:51 +0700

    Add files via upload

--- cypress/integration/manual-test/validate-TimesheetDashboard.test.js
+++ cypress/integration/manual-test/validate-TimesheetDashboard.test.js
@@ -4,14 +4,40 @@ describe('Check timesheet dashboard', () => {
         let userID = '6576'
         let userPasss = 'qa@123'
 
-        //check data return from API
+        //check user information data return from API
 
         cy.intercept('/api/user/login').as('getUserInfor')
+        cy.intercept('/api/timesheet*').as('getTimesheet')
         cy.login(userID, userPasss)
-        cy.wait('@getUserInfor').then(console.log)//its('response.body').should('include', `${userID}`)  
-        cy.get('@getUserInfor').should('have.data', `${userID}`)
+        /*cy.wait(['@getUserInfor', '@getTimesheet']).spread(
+            (getUserInfor, getTimesheet) => {
+                cy.get('@getUserInfor').should('have',`${userID}`)
+                cy.get('@getTimesheet').should('have',`${userID}`)
 
-        
+            })  */    
+            cy.wait('@getUserInfor').then((data) => {
+                cy.log(JSON.stringify(data.response.body))
+                // javascript handle data
+                let badgeNumber = data.response.body["data"].["badgeNumber"]
+                let name = data.response.body["data"].["name"]
+                let totalRemain = data.response.body["data"].["totalRemain"]
+                cy.log(badgeNumber, name, totalRemain)
+                let totalDays = Math.floor(totalRemain/8)
+                let totalHrs = totalRemain%8
+                if (totalHrs > 0){
+                    cy.contains(`${totalDays} days ${totalHrs} hours`)
+                } else {
+                    cy.contains(`${totalDays} days`)
+                }
+                cy.expect(userID).equal(badgeNumber)
+                cy.contains(name)
+                //cy.stickyVariable('quote', quote);
+                //cy.stickyVariable('author', author);
+                //cy.get('[data-test="quote"]').should('have.text', quote);
+                //cy.get('[data-test="quote-author"]').should('have.text', author);
+              })
+
+            
     })
 
     /*it('check left menu', () => {
